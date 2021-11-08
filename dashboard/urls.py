b6611d6cfde4266b86074c174da1e1ee4a8c2ce6
Sabbir Ahmed <38248492+sabbir1021@users.noreply.github.com>
2021-11-08 20:22:27 +0600

    remove html extrafile

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,5 +7,6 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.StafAdminView.as_view(), name="dashboards"),
     path('order_details/<int:id>', views.OrderDetailsView.as_view(), name="order_details"),
+    path('pdf_create<int:id>', views.create_pdf, name="pdf_create"),
     
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,15 @@ from django.views.generic.base import View
 from django.views import View 
 from cart.models import Order,OrderItem
 from django.contrib.auth.mixins import UserPassesTestMixin
-
+#pdf generate
+# import io
+# from django.http import FileResponse
+# from reportlab.pdfgen import canvas
+# from reportlab.lib.units import inch
+# from reportlab.lib.pagesizes import letter
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 # Create your views here.
 
 
@@ -47,3 +55,24 @@ class OrderDetailsView(View):
                 order.complete_order = True
                 order.save()
                 return redirect('dashboard:dashboards')
+
+
+def create_pdf(request,id):
+    order = get_object_or_404(Order, pk=id)
+    items = order.orderitem_set.all()
+    payment = order.payment_set.all()
+    template_path = 'dashboard/order_details_pdf.html'
+    context = {'order': order , 'items':items, 'payment':payment }
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_details_report.pdf"'
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
