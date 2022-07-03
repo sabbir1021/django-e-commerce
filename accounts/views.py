e46bb7e79fee5480b6429e6713e2f5a2f78cb537
sabbir1021 <5boysbd@gmail.com>
2022-07-03 16:20:32 +0600

    templates bug fix

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from .models import Profile,BillingAddress, ShippingAddress
 from cart.models import Order
 #token import
 from accounts.utils.email import send_account_confirmation_email
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode    
 from accounts.token import account_activation_token
 
